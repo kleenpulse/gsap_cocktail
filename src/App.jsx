5394653a9de2f36e002487480a5e21b93f37ccae
Vxrcel <skyeparagon@gmail.com>
2025-06-21 14:51:09 +0100

    add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/nav-bar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
-	return <main className="flex-center h-dvh">GSAP</main>;
+	return (
+		<main className="">
+			<Navbar />
+		</main>
+	);
 }
 
 export default App;
---
+++ src/components/nav-bar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { navLinks } from "../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+export default function Navbar() {
+	useGSAP(() => {
+		const navTween = gsap.timeline({
+			scrollTrigger: {
+				trigger: "nav",
+				start: "bottom top",
+			},
+		});
+
+		navTween.fromTo(
+			"nav",
+			{
+				backgroundColor: "transparent",
+			},
+			{
+				backgcroundColor: "#00000050",
+				backgroundFilter: "blur(10px)",
+				duration: 1,
+				ease: "power1.inOut",
+			}
+		);
+	});
+
+	return (
+		<nav>
+			<div className="flex">
+				<a href="#" className="flex gap-2 items-center">
+					<p>Cocktaila</p>
+				</a>
+
+				<ul>
+					{navLinks.map((link) => (
+						<li key={link.id} className="inline-block mx-4">
+							<a href={`#${link.id}`} className="">
+								{link.title}
+							</a>
+						</li>
+					))}
+				</ul>
+			</div>
+		</nav>
+	);
+}
